6ace6f61ada087d5c1f4ba0e3793760ddcdaafe9
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-09-21 22:53:58 +0200

    Updating the module.exports object in webpack.config.jsion file to tell the dev server where to look for files

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
  module.exports = {
+  mode: 'development',
    entry: {
      index: './src/index.js',
    },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
